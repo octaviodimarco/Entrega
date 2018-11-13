bbb4fc430825fc3c106b5746d6118e0fe672828c
octaviodimarco <1610251@ucc.edu.ar>
2018-11-13 09:55:02 -0300

    Comite 3

--- CLion/Proyecto/main.cpp
+++ CLion/Proyecto/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
+#include <stdio.h>
 
 #include "../Colisiones/OpenHashMap.h"
 //#include "../HashMap/HashMapLista.h"
 
 using namespace std;
+
+
 
 unsigned int sad(string clave) {
     unsigned int i=clave.length();
@@ -18,7 +22,7 @@ unsigned int sad(string clave) {
     return res;
 }
 
-int main(argc,argv) {
+int main() {
 //ofstream = salida
 //ifstream = entrada
 
@@ -43,9 +47,58 @@ int main(argc,argv) {
         fe >> convertida;
     }
 
-    cout<<"La clave de presente es "<<hm.get("la")<<endl;
+    cout<<"La clave de presente es "<<hm.get("casa")<<endl;
 
     fe.close();
+
+
+    ifstream despacito("../../Datos.txt");
+    ofstream entrega("../../Pepito.txt");
+
+    int b,k;
+    string linea;
+    string palabras[100];
+
+        while (!despacito.eof()){
+
+//       despacito>>palabra;
+            getline(despacito,linea);
+           // palabras = new string[linea.length()/2];
+
+            b=0;
+            k=0;
+            int j=0;
+            //linea[linea.length()]='\0';
+
+
+
+
+            for (int i = 0; i < linea.length()+1 ; ++i) {
+                if (linea[i] == ' ' || linea[i] == '\n' || linea[i] == ','|| linea[i] == '\0' ){
+                    palabras[b] = linea.substr(k, i-k);
+                    b++;
+                    k=i+1;
+                }
+            }
+
+            for (int j = 0; j < b; ++j) {
+                if(palabras[j].length()){
+                    size_t inicio = linea.find(palabras[j]);
+                    linea.replace(inicio, palabras[j].length(), hm.get(palabras[j]));
+                }
+            }
+
+            entrega<<linea<<endl;
+
+//                    entrega<<hm.get(palabra);
+//                    entrega<<" ";
+        }
+
+//            for (int i = 0; i < palabra.length() ; ++i) {
+//                if (palabra[i] == a[0])
+//
+//            }
+//
     return 0;
 
 
